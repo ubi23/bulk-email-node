728a58c95f3209dba94f03a92077d3fdb675226d
Ubaidullah Khan <khanubaid97.uk@gmail.com>
2021-02-02 14:47:03 +0100

    Implemented a form with POST method

--- node/routes/index.js
+++ node/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 let index = require('../controllers/index'); 
+let bulksendgrid = require('../controllers/bulksendgrid')
 
 /* GET home page. */
 router.get('/', index.index);
 
+/* POST home page */
+router.post('/', bulksendgrid.send)
 module.exports = router;
