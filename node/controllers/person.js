40fa6928d37463ea285e8462fda3c0184632dbc1
Ubaidullah Khan <khanubaid97.uk@gmail.com>
2021-02-03 16:35:31 +0100

    created Person class

---
+++ node/controllers/person.js
@@ -0,0 +1,21 @@
+class Person {
+  constructor(first_name, last_name, email){
+    this.name = first_name + " " + last_name;
+    if(this.isValidEmail(email)){
+      this.email = email;
+    }
+    
+  }
+
+  isValidEmail(email){
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))
+    {
+      return (true);
+    }
+    return false;
+  }
+}
+
+module.exports = {
+  Person: Person
+}
