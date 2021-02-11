06c238c6a142382c915231aea37ac29d53abcaef
ubi23 <khanubaid97.uk@gmail.com>
2021-02-11 19:08:41 +0100

    sending multiple emails works

--- node/controllers/sendgrid.controllers.js
+++ node/controllers/sendgrid.controllers.js
@@ -1,31 +1,82 @@
 // import sendgrid API
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+//const sgMail = require('@sendgrid/mail');
 
 
 module.exports.sendEmail = (recipients, data) => {
-  
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  sgMail.setSubstitutionWrappers('--', '--');
+
+  /*recipients.forEach(element => {
+    console.log("show sth",element);
+  });*/
   const msg = {
-    to: ['khanubaid97.uk@gmail.com'], // recipients
-    from: {'email': data.fromEmail, 'name': data.fromName}, 
-    reply_to: {'email': 'business@equalsmoney.com', 'name': 'Equals Money'},
+    personalizations: [
+      {
+        "to": [
+          {
+            "email": recipients[0].email,
+            "name": recipients[0].name
+          }
+        ],
+        "subject": "Example 01",
+        "substitutions": {
+          "firstName": "John"
+        }
+      },
+      {
+        "to": [
+          {
+            "email": recipients[1].email,
+            "name": "Jane"
+          }
+        ],
+        "subject": "Example 02",
+        "substitutions": {
+          "firstName": "Jane"
+        }
+      },
+      {
+        "to": [
+          {
+            "email": recipients[2].email,
+            "name": "Matt"
+          }
+        ],
+        "subject": "Example 03",
+        "substitutions": {
+          "firstName": "Matt"
+        }
+      }
+    ],
+    from: {email: data.fromEmail, name: data.fromName}, 
+    reply_to: {email: 'business@equalsmoney.com', name: 'Equals Money'},
     subject: data.subject,
-    templateID: data.templateID,
+    template_id: data.templateID,
+    //substitutions:  {"firstName": 'UbaidTest3'},
     categories: [data.category],
-    html: ''
+    html: 'Make sure this is never empty, otherwise you will get an error'
   };
 
   
-  //ES8
-  (async () => {
-    try {
-      await sgMail.send(msg);
-    } catch (error) {
-      console.error(error);
+  sgMail
+  .sendMultiple(msg)
+  .then(() => {
+    // Celebrate
+    console.log('Successsssss');
+  })
+  .catch(error => {
+    // Log friendly error
+    console.error(error);
 
-      if (error.response) {
-        console.error(error.response.body)
-      }
+    if (error.response) {
+      // Extract error msg
+      const {message, code, response} = error;
+
+      // Extract response msg
+      const {headers, body} = response;
+
+      console.error(body);
     }
-  })();
+  });
 }
