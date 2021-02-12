e1da580af9ce73d93b7e26ea6ebfed125b6f6fd7
ubi23 <khanubaid97.uk@gmail.com>
2021-02-12 12:29:24 +0100

    created helper function to upper case first letter of each word in a string

---
+++ node/controllers/helpers/uppercaseFirstLetterWordString.js
@@ -0,0 +1,14 @@
+module.exports = function uppercaseWordsString(str){
+    if(typeof str !== 'string' || typeof str === undefined){
+        throw new Error('String expected for conversion');
+    }
+    
+    const words = str.split(' ');
+    for(let i = 0; i<words.length; i++){
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+    }
+
+    return words.join(' ');
+}
+
+
