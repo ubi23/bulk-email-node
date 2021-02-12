4c2bc2406a024962140a66d829e0f3a415b39a50
ubi23 <khanubaid97.uk@gmail.com>
2021-02-12 13:02:41 +0100

    created personalisations for each recipient

--- node/controllers/sendgrid.controllers.js
+++ node/controllers/sendgrid.controllers.js
@@ -1,59 +1,19 @@
-// import sendgrid API
-//const sgMail = require('@sendgrid/mail');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setSubstitutionWrappers('--', '--');
 
 
 module.exports.sendEmail = (recipients, data) => {
-  const sgMail = require('@sendgrid/mail');
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  sgMail.setSubstitutionWrappers('--', '--');
-
-  /*recipients.forEach(element => {
-    console.log("show sth",element);
-  });*/
+  
+  // TODO
+  const personalizations = createPersonalization(recipients);
+  
   const msg = {
-    personalizations: [
-      {
-        "to": [
-          {
-            "email": recipients[0].email,
-            "name": recipients[0].name
-          }
-        ],
-        "subject": "Example 01",
-        "substitutions": {
-          "firstName": "John"
-        }
-      },
-      {
-        "to": [
-          {
-            "email": recipients[1].email,
-            "name": "Jane"
-          }
-        ],
-        "subject": "Example 02",
-        "substitutions": {
-          "firstName": "Jane"
-        }
-      },
-      {
-        "to": [
-          {
-            "email": recipients[2].email,
-            "name": "Matt"
-          }
-        ],
-        "subject": "Example 03",
-        "substitutions": {
-          "firstName": "Matt"
-        }
-      }
-    ],
+    personalizations: personalizations,
     from: {email: data.fromEmail, name: data.fromName}, 
     reply_to: {email: 'business@equalsmoney.com', name: 'Equals Money'},
     subject: data.subject,
     template_id: data.templateID,
-    //substitutions:  {"firstName": 'UbaidTest3'},
     categories: [data.category],
     html: 'Make sure this is never empty, otherwise you will get an error'
   };
@@ -79,4 +39,22 @@ module.exports.sendEmail = (recipients, data) => {
       console.error(body);
     }
   });
+}
+
+
+function createPersonalization(recipients){
+  if(typeof recipients === undefined || typeof recipients === 'string'){
+    return;
+  }
+
+  let personalizations = [];
+  recipients.forEach((recipient) => { 
+    personalizations.push(
+      {
+        'to': [recipient],
+        'substitutions': {'firstName': recipient.name}
+      }
+    );
+  });
+  return personalizations;
 }
