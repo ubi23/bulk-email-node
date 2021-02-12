e556ea2550b2b70625154926bdc7df242cab7963
Ubaidullah Khan <35372700+ubi23@users.noreply.github.com>
2021-02-12 15:25:08 +0100

    Deleted node-tutorial directory

--- node/node-tutorial/myfirstmodule.js
+++
@@ -1,6 +0,0 @@
-// you can create a module
-// this creates a module that returns a date and a time object
-exports.myDateTime = function() {
-  return Date();
-};
-// use exports keyword to make properties and methods available outside the module file  
