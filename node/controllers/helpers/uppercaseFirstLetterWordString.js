aec0c3667dad592a53ade9fd019d602eac7034c1
ubi23 <khanubaid97.uk@gmail.com>
2021-02-12 14:46:13 +0100

    added comments explaining functionality

--- node/controllers/helpers/uppercaseFirstLetterWordString.js
+++ node/controllers/helpers/uppercaseFirstLetterWordString.js
@@ -1,3 +1,7 @@
+/**
+ * Make the first letter of each word in a string uppercase 
+ * an lowercase the rest. 
+ */
 module.exports = function uppercaseWordsString(str){
     if(typeof str !== 'string' || typeof str === undefined){
         throw new Error('String expected for conversion');
@@ -7,8 +11,5 @@ module.exports = function uppercaseWordsString(str){
     for(let i = 0; i<words.length; i++){
         words[i] = words[i][0].toUpperCase() + words[i].substr(1).toLowerCase();
     }
-
     return words.join(' ');
 }
-
-
