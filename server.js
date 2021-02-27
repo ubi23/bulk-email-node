5cf400026104fdecadbc6b80c6be4b78a0363ade
ubi23 <khanubaid97.uk@gmail.com>
2021-02-27 12:34:34 +0000

    created new server.js file

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const indexRouter = require('./routes/index')
+const path = require('path');
+// instance of express app
+const app = express(); 
+
+const start = async () => {
+  
+  // parse requests of content-type - application/json
+  app.use(bodyParser.json());
+  
+  // parse requests of content-type - application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+
+  // use this middleware for all requests
+  app.use('/', indexRouter);
+   
+  // set port, listen for requests
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
+};
+
+(async () => await start())();
