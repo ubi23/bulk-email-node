0307f5d609922636926c46b5bfe71bf061390597
ubi23 <khanubaid97.uk@gmail.com>
2021-03-04 09:38:07 +0000

    trying to work with handlebars but it doesn't work

--- server.js
+++ server.js
@@ -2,20 +2,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index')
 const path = require('path');
+const exphbs = require('express-handlebars');
 // instance of express app
 const app = express(); 
 
 const start = async () => {
   
+  app.use(express.static(__dirname + '/public'));
+
   // parse requests of content-type - application/json
   app.use(bodyParser.json());
   
   // parse requests of content-type - application/x-www-form-urlencoded
   app.use(bodyParser.urlencoded({ extended: true }));
-
+  
   // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
+  app.set('view engine', 'hbs');
+  app.engine('hbs', exphbs({
+    extname: 'hbs',
+    defaultLayout: 'index',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/',
+
+    helpers: {
+      ifEq: function(key, param, options){
+        console.log('options = ', options);
+        console.log('param = ', param);
+        console.log('key = ', key);
+        if(param == key){
+          //console.log('value is  = ', value);
+          return true;
+        }
+      }
+    }
+  }));
 
   // use this middleware for all requests
   app.use('/', indexRouter);
