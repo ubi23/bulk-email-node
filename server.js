3ecedc7716e80575d417da4737ba49d1f9436040
ubi23 <khanubaid97.uk@gmail.com>
2021-03-09 10:53:47 +0000

    db setup + migration

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index')
 const path = require('path');
+const db = require('./models');
 // instance of express app
 const app = express(); 
 
@@ -24,6 +25,10 @@ const start = async () => {
   const PORT = process.env.PORT || 3000;
 
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
+
+  process.on('exit', function () {
+    sequelize.connectionManager.close().then(() => console.log('Shut down gracefully'));
+  });
 };
 
 (async () => await start())();
