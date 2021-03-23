ce9af2ef6aa2722e5a12d78678e154a1fc282367
ubi23 <khanubaid97.uk@gmail.com>
2021-03-23 11:53:41 +0000

    test(unit): created unit test for toCamelCase and toSnakeCase helper functions

--- controllers/helpers/helpers/str-change-case.test.js
+++ controllers/helpers/helpers/str-change-case.test.js
@@ -1,19 +1,64 @@
 const {toCamelCase, toSnakeCase} = require('./str-change-case');
 
-test('string with spaces converts to camel case', () => {
-  const objWithKeyContainingSpaces = {
+let objWithKeys = {};
+
+beforeAll(() => {
+  objWithKeys = {
     'key 1 with space': 1,
     ' Second keySpaced': 2,
     'Third_key': 3,
     '      fourth _key    ': 4,
     'Key_____five': 5
   };
-  const expectedObj = {
+});
+
+test('object\'s keys convert to camel case', () => {
+  
+  let expectedObj = {
     'key1WithSpace': 1,
     'secondKeySpaced': 2,
     'thirdKey': 3,
     'fourthKey': 4,
     'keyFive': 5
   };
-  expect(toCamelCase(objWithKeyContainingSpaces)).toEqual(expectedObj);
+
+  expect(toCamelCase(objWithKeys)).toEqual(expectedObj);
+});
+
+test('toCamelCase function throws error as non object passed for conversion', () => {
+  let str = '';
+
+  expect(() => toCamelCase(str)).toThrow();
+  expect(() => toCamelCase(str)).toThrow(Error);
+  expect(() => toCamelCase(str)).toThrow('Non object passed to convertKeys: ' + str);
+  expect(() => toCamelCase(undefined)).toThrow('Non object passed to convertKeys: ' + undefined);
+  expect(() => toCamelCase(null)).toThrow('Non object passed to convertKeys: ' + null);
+  expect(() => toCamelCase(true)).toThrow('Non object passed to convertKeys: ' + true);
+  expect(() => toCamelCase(false)).toThrow('Non object passed to convertKeys: ' + false);
+  expect(() => toCamelCase(1)).toThrow('Non object passed to convertKeys: ' + 1);
+});
+
+test('object\'s keys convert to snake case', () => {
+  
+  let expectedObj = {
+    'key1_with_space': 1,
+    'second_key_spaced': 2,
+    'third_key': 3,
+    'fourth_key': 4,
+    'key_five': 5
+  };
+  expect(toSnakeCase(objWithKeys)).toEqual(expectedObj);
+});
+
+test('toSnakeCase function throws error as non object passed for conversion', () => {
+  let str = '';
+
+  expect(() => toSnakeCase(str)).toThrow();
+  expect(() => toSnakeCase(str)).toThrow(Error);
+  expect(() => toSnakeCase(str)).toThrow('Non object passed to convertKeys: ' + str);
+  expect(() => toSnakeCase(undefined)).toThrow('Non object passed to convertKeys: ' + undefined);
+  expect(() => toSnakeCase(null)).toThrow('Non object passed to convertKeys: ' + null);
+  expect(() => toSnakeCase(true)).toThrow('Non object passed to convertKeys: ' + true);
+  expect(() => toSnakeCase(false)).toThrow('Non object passed to convertKeys: ' + false);
+  expect(() => toSnakeCase(1)).toThrow('Non object passed to convertKeys: ' + 1);
 });
